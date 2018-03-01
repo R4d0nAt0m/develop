c39c2c885abb81548767feef384d58d229b6b8ec
R4d0nAt0m <r4d0n3m4il@gmail.com>
2018-03-01 14:18:18 +0000

    Added sum

---
+++ Math.java
@@ -0,0 +1,18 @@
+public class Math{
+
+  private int a;
+  private int b;
+
+  public Math(int a, int b){
+
+    this.a = a;
+    this.b = b;
+
+  }
+
+  public int sum(){
+    return a+b;
+  }
+
+
+}
